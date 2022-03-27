a5981ad1dbac4a8ce72221f15428b6b5d4d6c8c2
MauriMiguez <mauri.miguez@hotmail.com>
2022-03-27 13:14:14 -0300

    Feat: created state and cubit for managing the states and atributes of create category

---
+++ lib/create_category/bloc/create_category_state.dart
@@ -0,0 +1,44 @@
+part of 'create_category_cubit.dart';
+
+abstract class CreateCategoryState {
+  Color color = Colors.red;
+  String name = '';
+}
+
+class CreateCategoryInitial extends CreateCategoryState {}
+
+class ChangeField extends CreateCategoryState {
+
+  ChangeField({name = String, color = Color}){
+    this.name = name;
+    this.color = color;
+  }
+}
+
+class NameError extends CreateCategoryState {
+  final String nameError;
+  NameError({required this.nameError, name = String, color = Color} ){
+    this.name = name;
+    this.color = color;
+  }
+}
+
+
+class CreateCategoryError extends CreateCategoryState {
+  final String error;
+  CreateCategoryError({required this.error, name = String, color = Color}){
+    this.name = name;
+    this.color = color;
+  }
+}
+
+class CreateCategoryLoading extends CreateCategoryState {
+  CreateCategoryLoading({name = String, color = Color}){
+    this.name = name;
+    this.color = color;
+  }
+}
+
+class CreateCategoryLoaded extends CreateCategoryState {}
+
+
